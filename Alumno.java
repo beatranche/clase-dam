ea7e49d1b4ef3e30229e32abd1d07d5cd911093d
beatranche <beatranche@users.noreply.github.com>
2019-11-03 19:08:48 +0100

    Se genera un nombre de usuario para GitHub aunque no tenga la longitud requerida

--- Alumno.java
+++ Alumno.java
@@ -1,11 +1,13 @@
 public class Alumno {
-    
+
     // el nombre completo del alumno
     private String nombre;
     // el numero de matricula
     private String numeroMatricula;
     // la edad del alumno
     private int edad;
+    
+    
 
     /**
      * Crea un alumno nuevo
@@ -14,18 +16,17 @@ public class Alumno {
         nombre = nombreCompleto;
         numeroMatricula = numeroMatriculaAlumno;
         edad = edadAlumno;
-        
+
         if (nombre.length() < 3){
-            System.out.println ("Error, se necesitan mas de tres caracteres");
-       
+            System.out.println ("Error, se necesitan mas de tres caracteres en el nombre");
+
         }
-        else if(numeroMatricula.length() < 4) {
-             System.out.println ("Error, se necesitan mas de cuatro caracteres");
-        
+        if(numeroMatricula.length() < 4) {
+            System.out.println ("Error, se necesitan mas de cuatro caracteres en la matricula");
+
         }
-    
+ 
     }
-    
 
     /**
      * Devuelve el nombre completo del alumno
@@ -33,26 +34,32 @@ public class Alumno {
     public String getNombre() {
         return nombre;
     }
-    
+
     /**
      * Cambia el nombre del alumno
      */
     public void cambiarnombre(String nuevoNombre) {
         nombre = nuevoNombre;
     }
-    
+
     /**
      * Imprime por pantalla los detalles del alumno
      */
     public void imprimeDetalles() {
         System.out.println(nombre + " (" + numeroMatricula + ") - " + edad + " años");
     }
-    
+
     /**
      * Devuelve el nombre de usuario que el alumno debe configurar
      * en su cuenta de Github en formato de 7 caracateres
      */
     public String getNombreUsuarioGithub() {
-        return nombre.substring(0, 3) + numeroMatricula.substring(0, 4);
+        if (nombre.length() < 3 || numeroMatricula.length() < 4){
+        nombre = nombre + "_" +  numeroMatricula;
+        numeroMatricula = numeroMatricula + "     ";
+               
+    }
+        return  nombre.substring(0,3) +  numeroMatricula.substring (0,4); 
     }
 }
+
